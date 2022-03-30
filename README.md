c9e45830aeeba6014d36bea547f22d67dae218ad
Aryane Valentim <88635265+AryaneValentim@users.noreply.github.com>
2022-03-30 16:47:13 -0300

    Add files via upload

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# MeuApp
+# ProjetoAngular
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 13.3.0.
 
--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/meu_app'),
+      dir: require('path').join(__dirname, './coverage/projeto-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
